d622e3949ae3e0531fe0da431212d6e919d5adb1
Andrey Tsarevskiy <tsarevskiy@haulmont.com>
2014-08-27 12:59:30 +0000

    Remove gantt chart from charts project #PL-4241

--- modules/web6/src/com/haulmont/charts/web/gui/ChartComponentPalette.java
+++ modules/web6/src/com/haulmont/charts/web/gui/ChartComponentPalette.java
@@ -6,10 +6,6 @@
 
 package com.haulmont.charts.web.gui;
 
-import com.haulmont.charts.gui.components.charts.GanttChart;
-import com.haulmont.charts.gui.xml.layout.loaders.charts.GanttChartLoader;
-import com.haulmont.charts.web.gui.components.charts.jsgantt.WebJSGanttChart;
-import com.haulmont.charts.web.toolkit.ui.charts.jsgantt.JSGanttChart;
 import com.haulmont.cuba.gui.ComponentPalette;
 import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.gui.xml.layout.ComponentLoader;
@@ -29,16 +25,12 @@ public class ChartComponentPalette implements ComponentPalette {
 
         Map<String, Class<? extends ComponentLoader>> loaders = new HashMap<>();
 
-        loaders.put("ganttChart", GanttChartLoader.class);
-
         return loaders;
     }
 
     @Override
     public Map<String, Class<? extends Component>> getComponents() {
         Map<String, Class<? extends Component>> components = new HashMap<>();
-
-        components.put(GanttChart.NAME + "@" + JSGanttChart.VENDOR, WebJSGanttChart.class);
 
         return components;
     }
