48b4fd263a71b99bd45644e38233760dc59d6538
Yuriy Artamonov <artamonov@haulmont.com>
2015-10-14 18:04:58 +0000

    Reports with chart does not display chart #PL-6166

--- modules/web/src/com/haulmont/charts/web/ui/JsonChartController.java
+++ modules/web/src/com/haulmont/charts/web/ui/JsonChartController.java
@@ -5,27 +5,40 @@
 package com.haulmont.charts.web.ui;
 
 import com.haulmont.charts.gui.amcharts.model.charts.PieChart;
-import com.haulmont.charts.web.gui.components.charts.amcharts.WebChart;
+import com.haulmont.charts.gui.components.charts.Chart;
 import com.haulmont.charts.web.toolkit.ui.amcharts.CubaAmchartsScene;
+import com.haulmont.cuba.gui.WindowParam;
 import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
+import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
 
+import javax.inject.Inject;
 import java.util.Map;
 
 /**
  * @author degtyarjov
+ * @version $Id$
  */
 public class JsonChartController extends AbstractFrame {
+
+    @WindowParam(required = true)
+    protected String chartJson;
+
+    @Inject
+    protected ComponentsFactory componentsFactory;
+
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
-        String chartJson = (String) params.get("Chart");
-        WebChart webChart = new WebChart();
-        webChart.setHeight("100%");
-        webChart.setWidth("100%");
-        CubaAmchartsScene cubaAmchartsScene = (CubaAmchartsScene) webChart.getComponent();
+
+        Chart chart = (Chart) componentsFactory.createComponent(Chart.NAME);
+        chart.setHeight("100%");
+        chart.setWidth("100%");
+
+        CubaAmchartsScene cubaAmchartsScene = (CubaAmchartsScene) WebComponentsHelper.unwrap(chart);
         cubaAmchartsScene.drawChart(new PieChart());
         cubaAmchartsScene.setJson(chartJson);
-        cubaAmchartsScene.drawChart();
-        add(webChart);
+
+        add(chart);
     }
 }
