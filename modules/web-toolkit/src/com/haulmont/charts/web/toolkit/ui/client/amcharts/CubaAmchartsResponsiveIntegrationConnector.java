c2fb6054286ad8582cd5b25aa52188b44f230d96
Yuriy Artamonov <artamonov@haulmont.com>
2017-03-28 12:58:26 +0400

    PL-8854 Client side error - ChartsWidgetSet does not contain an implementation for CubaAmchartsResponsiveIntegration

---
+++ modules/web-toolkit/src/com/haulmont/charts/web/toolkit/ui/client/amcharts/CubaAmchartsResponsiveIntegrationConnector.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2008-2017 Haulmont. All rights reserved.
+ * Use is subject to license terms, see http://www.cuba-platform.com/commercial-software-license for details.
+ */
+
+package com.haulmont.charts.web.toolkit.ui.client.amcharts;
+
+import com.haulmont.charts.web.toolkit.ui.amcharts.CubaAmchartsResponsiveIntegration;
+import com.vaadin.client.ServerConnector;
+import com.vaadin.client.extensions.AbstractExtensionConnector;
+import com.vaadin.shared.ui.Connect;
+
+@Connect(CubaAmchartsResponsiveIntegration.class)
+public class CubaAmchartsResponsiveIntegrationConnector extends AbstractExtensionConnector {
+    @Override
+    protected void extend(ServerConnector target) {
+        // do nothing
+        // this extension simply adds JavaScript include
+    }
+}
