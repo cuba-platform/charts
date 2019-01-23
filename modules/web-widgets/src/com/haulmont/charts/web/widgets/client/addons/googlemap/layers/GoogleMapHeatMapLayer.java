43a3511f2e8e3a2b1252fdc8895c03f334871686
Yuriy Artamonov <artamonov@haulmont.com>
2019-01-23 23:20:48 +0400

    Apache License 2.0

--- modules/web-widgets/src/com/haulmont/charts/web/widgets/client/addons/googlemap/layers/GoogleMapHeatMapLayer.java
+++ modules/web-widgets/src/com/haulmont/charts/web/widgets/client/addons/googlemap/layers/GoogleMapHeatMapLayer.java
@@ -1,6 +1,17 @@
 /*
- * Copyright (c) 2008-2016 Haulmont. All rights reserved.
- * Use is subject to license terms, see http://www.cuba-platform.com/commercial-software-license for details.
+ * Copyright (c) 2008-2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.haulmont.charts.web.widgets.client.addons.googlemap.layers;
@@ -12,9 +23,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Igor Korotkov (igor@ikorotkov.com)
- */
 public class GoogleMapHeatMapLayer implements Serializable {
     private static final long serialVersionUID = 896513236735515400L;
 
@@ -22,8 +30,8 @@ public class GoogleMapHeatMapLayer implements Serializable {
 
     private long id;
 
-    private List<LatLon> data = new ArrayList<LatLon>();
-    private List<WeightedLocation> weightedData = new ArrayList<WeightedLocation>();
+    private List<LatLon> data = new ArrayList<>();
+    private List<WeightedLocation> weightedData = new ArrayList<>();
     private List<String> gradient;
     private Double opacity = 0.6;
     private Double radius;
