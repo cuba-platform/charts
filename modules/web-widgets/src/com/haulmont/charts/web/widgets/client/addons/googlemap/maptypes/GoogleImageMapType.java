34f425f63f3486aa8e3445252309920078b73fd4
Yuriy Artamonov <artamonov@haulmont.com>
2019-01-23 23:20:48 +0400

    Apache License 2.0

--- modules/web-widgets/src/com/haulmont/charts/web/widgets/client/addons/googlemap/maptypes/GoogleImageMapType.java
+++ modules/web-widgets/src/com/haulmont/charts/web/widgets/client/addons/googlemap/maptypes/GoogleImageMapType.java
@@ -1,6 +1,17 @@
 /*
- * Copyright (c) 2008-2016 Haulmont. All rights reserved.
- * Use is subject to license terms, see http://www.cuba-platform.com/commercial-software-license for details.
+ * Copyright (c) 2008-2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.haulmont.charts.web.widgets.client.addons.googlemap.maptypes;
@@ -9,10 +20,6 @@ import com.haulmont.charts.web.widgets.client.addons.googlemap.base.Size;
 
 import java.io.Serializable;
 
-/**
- * @author korotkov
- * @version $Id$
- */
 public class GoogleImageMapType implements Serializable {
 
     private static final long serialVersionUID = 452356543641L;
